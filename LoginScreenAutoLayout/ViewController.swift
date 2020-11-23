1466793792d7f611112c0e95d634d4a499ec1c3d
Katie Embrey-Farquhar <kembreyfarquhar@gmail.com>
2020-11-23 00:41:07 -0600

    Initial Commit

---
+++ LoginScreenAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginScreenAutoLayout
+//
+//  Created by Katie on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
